f94df29a34711117ffd8ae1f3065d49e8f26688e
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-04-22 20:11:12 +0200

    Update rpiplay-boot.sh

--- rpiplay-boot.sh
+++ rpiplay-boot.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo wget -O /etc/init.d/rpiplay-boot-script.sh https://raw.githubusercontent.com/TRMSC/linux-quickinstalls/rpiplay-standalone/src/rpiplay-boot-script.sh
 sudo chmod 755 /etc/init.d/rpiplay-boot-script.sh
 sudo update-rc.d rpiplay-boot-script.sh defaults
