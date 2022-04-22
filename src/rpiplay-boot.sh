f2d6cc7c07d881a5d98ad807350196b5e04e70f3
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-04-22 19:54:13 +0200

    Add exit possibility

--- src/rpiplay-boot.sh
+++ src/rpiplay-boot.sh
@@ -1,30 +1,12 @@
-#! /bin/sh
+#!/bin/bash
 
-# https://tutorials-raspberrypi.de/raspberry-pi-autostart-programm-skript/
+REPLY=""
+read -p "Type anything and press enter to skip RPiPlay (5sec)..." -t 5
 
-### BEGIN INIT INFO
-# Provides: rpiplay
-# Required-Start: $syslog
-# Required-Stop: $syslog
-# Default-Start: 2 3 4 5
-# Default-Stop: 0 1 6
-# Short-Description: rpiplay
-# Description:
-### END INIT INFO
- 
-case "$1" in
-    start)
-        echo "Start rpiplay..."
-        /usr/local/bin/rpiplay
-        ;;
-    stop)
-        echo "Stop rpiplay..."
-        killall rpiplay
-        ;;
-    *)
-        echo "Use: /etc/init.d/rpiplay-boot.sh {start|stop}"
-        exit 1
-        ;;
-esac
- 
-exit 0
+if [ -z $REPLY ]
+then
+   echo "Start RPiPlay..."
+   /usr/local/bin/rpiplay
+else
+   echo "Skip RPiPlay..."
+fi
