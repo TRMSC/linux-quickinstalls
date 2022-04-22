eb16d00f31393eeeacdeb09a8e2e2e315b2d3865
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-04-22 20:20:15 +0200

    Add space

--- rpiplay-boot.sh
+++ rpiplay-boot.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#! /bin/bash
 
 sudo wget -O /etc/init.d/rpiplay-boot-script.sh https://raw.githubusercontent.com/TRMSC/linux-quickinstalls/rpiplay-standalone/src/rpiplay-boot-script.sh
 sudo chmod 755 /etc/init.d/rpiplay-boot-script.sh
